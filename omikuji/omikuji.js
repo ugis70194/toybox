b6a2ca679958d8489cf71de999ea13b1da88c924
ugis70194 <ugis70194@yahoo.co.jp>
2019-02-21 20:49:54 +0900

    webassemblytest

--- omikuji/omikuji.js
+++ omikuji/omikuji.js
@@ -5,14 +5,14 @@ var main = new Vue({
         show: false,
         res: ['大吉','大吉','末吉','末吉','末吉','吉','吉','凶','凶'],
         num: 0,
-        tweet: 'https://twitter.com/intent/tweet?hashtags=うぐいすのおみくじ&text=私の運勢は'+this.res[this.num]+'でした！&url=https://ugis70194.github.io/omikuji/.'
+        tweet: 'https://twitter.com/intent/tweet?hashtags=うぐいすのおみくじ&text=私の運勢は'+this.res[this.num]+'でした&url=https://ugis70194.github.io/omikuji/.'
     },
     methods: {
         choose: function() {
             this.show = !this.show
             if(this.show) {
                 this.num = Math.ceil(Math.random()*9-1)
-                this.tweet = 'https://twitter.com/intent/tweet?hashtags=うぐいすのおみくじ&text=私の運勢は'+this.res[this.num]+'でした！&url=https://ugis70194.github.io/omikuji/.'
+                this.tweet = 'https://twitter.com/intent/tweet?hashtags=うぐいすのおみくじ&text=私の運勢は'+this.res[this.num]+'でした&url=https://ugis70194.github.io/omikuji/.'
             }
         },
     }
